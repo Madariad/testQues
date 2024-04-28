7ffd1726515fc6887ca95cdd22b715895d1a8060
Dias <dias.baigazi05@gmail.com>
2024-04-29 03:37:42 +0500

    fix rout test?

--- src/main.jsx
+++ src/main.jsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter(
           element: <MainPage/>,
         },
         {
-          path: "/test/t",
+          path: "test/t",
           element: <TestListPage/>
         },
         ],
